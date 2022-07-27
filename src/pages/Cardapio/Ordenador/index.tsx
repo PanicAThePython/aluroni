ef68712b82aba5fbbabfd34396e9522f92370600
Natália Sens Weise <nataliaweise@gmail.com>
2022-07-27 16:06:58 -0300

    Using Eslint and creating new page

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -11,29 +11,29 @@ interface Props {
 
 
 export default function Ordenador({ ordenador, setOrdenador }: Props) {
-    const [aberto, setAberto] = useState(false);
-    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
-    return (
-        <button
-            className={classNames({
-                [styles.ordenador]: true,
-                [styles['ordenador--ativo']]: ordenador !== '',
-            })}
-            onClick={() => setAberto(!aberto)}
-            onBlur={() => setAberto(false)}>
-            <span>{nomeOrdenador || 'Ordenar Por'}</span>
-            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
-            <div className={classNames({
-                [styles.ordenador__options]: true,
-                [styles['ordenador__options--ativo']]: aberto,
-            }
-            )}>
-                {opcoes.map((op) => (
-                    <div className={styles.ordenador__option} key={op.value} onClick={() => setOrdenador(op.value)}>
-                        {op.nome}
-                    </div>
-                ))}
-            </div>
-        </button>
-    )
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  return (
+    <button
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles['ordenador--ativo']]: ordenador !== '',
+      })}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}>
+      <span>{nomeOrdenador || 'Ordenar Por'}</span>
+      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles['ordenador__options--ativo']]: aberto,
+      }
+      )}>
+        {opcoes.map((op) => (
+          <div className={styles.ordenador__option} key={op.value} onClick={() => setOrdenador(op.value)}>
+            {op.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
 }
