60299c3fd96a1dc779e1e1bb13052b76897db64c
Natália Sens Weise <nataliaweise@gmail.com>
2022-07-27 14:13:55 -0300

    Sending complete project

---
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,39 @@
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+
+interface Props {
+    ordenador: string,
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+}
+
+
+export default function Ordenador({ ordenador, setOrdenador }: Props) {
+    const [aberto, setAberto] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+    return (
+        <button
+            className={classNames({
+                [styles.ordenador]: true,
+                [styles['ordenador--ativo']]: ordenador !== '',
+            })}
+            onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)}>
+            <span>{nomeOrdenador || 'Ordenar Por'}</span>
+            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+            <div className={classNames({
+                [styles.ordenador__options]: true,
+                [styles['ordenador__options--ativo']]: aberto,
+            }
+            )}>
+                {opcoes.map((op) => (
+                    <div className={styles.ordenador__option} key={op.value} onClick={() => setOrdenador(op.value)}>
+                        {op.nome}
+                    </div>
+                ))}
+            </div>
+        </button>
+    )
+}
